e631757bbe2e07dbec06532170ac13d14a0fcc36
Leonardo <lpbaiser@gmail.com>
2016-09-18 16:33:06 -0300

    diagrama de classes

---
+++ Chat/src/chat/Client.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat;
+
+import java.net.MulticastSocket;
+
+/**
+ *
+ * @author leonardo
+ */
+public class Client {
+
+    private String idGroup;
+    private int port;
+    private String apelido;
+    private MulticastSocket multicastSocket;
+    
+    public Client() {
+        
+    }
+    
+}
